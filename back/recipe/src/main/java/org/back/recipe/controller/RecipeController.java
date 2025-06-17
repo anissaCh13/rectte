49070fc9985410dd1ab929bffd3bc048d8f7791a
anissachekir <anissa.chekir@glady.com>
2025-06-17 16:44:31 +0200

    (feat/RECP-16): creat the first endpoint(MR)

--- back/recipe/src/main/java/org/back/recipe/controller/RecipeController.java
+++ back/recipe/src/main/java/org/back/recipe/controller/RecipeController.java
@@ -1,7 +1,7 @@
 package org.back.recipe.controller;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.back.recipe.model.RecipeDTO;
 import org.back.recipe.services.RecipeService;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class RecipeController {
 
-  RecipeService recipeService;
+  private final RecipeService recipeService;
 
   @GetMapping("/recipes")
   public ResponseEntity<List<RecipeDTO>> getAllRecipes() {
--- back/recipe/src/main/java/org/back/recipe/services/implem/RecipeServiceImpl.java
+++ back/recipe/src/main/java/org/back/recipe/services/implem/RecipeServiceImpl.java
@@ -1,6 +1,5 @@
 package org.back.recipe.services.implem;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.back.recipe.model.RecipeDTO;
 import org.back.recipe.services.RecipeService;
@@ -12,9 +11,6 @@ public class RecipeServiceImpl implements RecipeService {
 
   @Override
   public List<RecipeDTO> getAllRecipes() {
-    List<RecipeDTO> recipes = new ArrayList<>();
-    RecipeDTO recipeDTO= RecipeDTO.builder().title("my first Recipe").description("my description").ingredients(List.of("piment", "sauce")).build();
-    recipes.add(recipeDTO);
-    return recipes;
+    return List.of(RecipeDTO.builder().title("my first Recipe").description("my description").ingredients(List.of("piment", "sauce")).build());
   }
 }
