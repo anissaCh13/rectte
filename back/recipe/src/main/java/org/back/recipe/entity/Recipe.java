3f3e2378a0c04183148ae801e64b8d5dc7038763
anissachekir <anissa.chekir@glady.com>
2025-06-18 12:49:08 +0200

    (feat/RECP-29): Creat connection with BDD and load all recipe (merge review)

--- back/recipe/src/main/java/org/back/recipe/entity/Recipe.java
+++ back/recipe/src/main/java/org/back/recipe/entity/Recipe.java
@@ -1,14 +1,27 @@
 package org.back.recipe.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import java.util.UUID;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Data
 public class Recipe {
 
+  @EqualsAndHashCode.Include
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+  @Column(updatable = false, nullable = false)
   @Id
   private UUID id;
   private String title;
