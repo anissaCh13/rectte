9bf9c0db7f9eb333562998c1ee1068659f736802
anissa <anissa.ch.cours@gmail.com>
2025-06-22 15:00:53 +0200

    (feat/RECP-45): creat header and footer component, and reorganize the structure of project

--- front/recipe-front-app/src/app/app.ts
+++ front/recipe-front-app/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { Header } from './core/layout/header/header';
+import { Footer } from './core/layout/footer/footer';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSlideToggle],
+  imports: [MatSlideToggle, Header, Footer, MatButton],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
